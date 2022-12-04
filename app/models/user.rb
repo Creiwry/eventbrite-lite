22eff5031bf3fa2dc810f1ecd2b0ac6f76348049
Creiwry <jay.gardos@outlook.com>
2022-12-04 17:00:23 +0100

    feat(models): add dependencies for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_many :events, foreign_key: 'administrator_id', class_name: 'Event'
+  has_many :attendances, foreign_key: 'participant_id', class_name: 'Attendance'
+  has_many :events, through: :attendances, foreign_key: 'participant_id', class_name: 'Attendance'
 end
